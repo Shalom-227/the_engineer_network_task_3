06cf710486cd6299058bd81e3a816804072c69c6
Shalom Alalade <alaladeshalom02@gmail.com>
2025-11-05 15:05:50 +0000

    populate localQuotes array with sample quote objects

--- src/components/random-quote/random-quote.jsx
+++ src/components/random-quote/random-quote.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 import "./random-quote.css"
 
 const RandomQuote = () => {
-     const localQuotes = [];
+     const localQuotes = [
+        { text: "If you tell the truth, you don't have to remember anything.",
+            author: "Mark Twain",
+        },
+         {text: "Yesterday is history, tomorrow is a mystery, today is a gift of God, which is why we call it the present.",
+            author: "Bill Keane",
+        },
+        {text: "It is never too late to be what you might have been.",
+            author: "George Eliot",
+        },
+        {text: "For every minute you are angry you lose sixty seconds of happiness.",
+            author: "Ralph Waldo Emerson",
+        },
+     ];
     return (
     
        

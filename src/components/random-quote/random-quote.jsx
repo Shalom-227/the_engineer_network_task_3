7a64e6d2317060d81d6cd35d972f5f69f1e6ed07
Shalom Alalade <alaladeshalom02@gmail.com>
2025-11-06 17:50:42 +0000

    add more quotes to localQuotes array

--- src/components/random-quote/random-quote.jsx
+++ src/components/random-quote/random-quote.jsx
@@ -17,6 +17,63 @@ const RandomQuote = () => {
         {text: "For every minute you are angry you lose sixty seconds of happiness.",
             author: "Ralph Waldo Emerson",
         },
+        {
+            text: "Faith loves the journey. Gratitude loves the moment. Inner peace loves the pause",
+            author: "Maxime Lagacé",
+        },
+        {
+            text: "Believe deep down in your heart that you’re destined to do great things.",
+            author: "Joe Paterno",
+        },
+        {
+            text: "One small positive thought can change your whole day",
+            author: "Zig Ziglar",
+        },
+        {
+           text: "Arise, awake and stop not till the goal is reached.",
+           author: "Swami Vivekananda",
+        },
+        {
+            text: "It doesn’t matter how slow you go, as long as you don’t stop",
+            author: "Confucius"
+
+        },
+        {
+            text: "Change your thoughts and you change your world.",
+            author: "Norman Vincent Peale",
+        },
+        {
+            text: "You cannot have a positive life and a negative mind.",
+            author: "Joyce Meyer",
+        },
+        {
+            text: "The best is yet to be.",
+            author: "Robert Browning",
+        },
+        {
+            text: "There are far, far better things ahead than anything we leave behind.",
+            author: "C.S.Lewis",
+        },
+        {
+           text: "Clear your mind of “cant”.",
+           author: "Samuel Johnson",
+        },
+        {
+            text: "It’s not a no, it’s a not yet. It’s not failure, it’s feedback. It’s not pain, it’s fuel for power.",
+            author: "Maxime Lagacé",
+        },
+        {
+           text: "The road to success is always under construction.",
+           author: "Lily Tomlin", 
+        },
+        {
+            text: "By crawling a child learns to stand.",
+            author: "African Proverb",
+        },
+        {
+            text: "Little by little the bird builds its nest.",
+            author: "Nigerian Proverb"
+        },
      ];
 
      const [quotes, setQuotes] = useState(localQuotes);
